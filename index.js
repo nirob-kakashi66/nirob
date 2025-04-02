a2fb3ebc908da23f97de17bdc6107864d780ec26
itachi-prime99 <ovhi.bbz108@outlook.com>
2025-04-03 00:49:03 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { spawn } = require("child_process"); const log = require("./logger/log.js
 
 function startProject() { log.info("Starting Project...");
 
-const process = spawn("nohup", ["node", "EREN.js"], {
+const process = spawn("pnpm", ["start"], {
     cwd: __dirname,
     stdio: "inherit",
     shell: true
@@ -11,7 +11,7 @@ const process = spawn("nohup", ["node", "EREN.js"], {
 process.on("exit", (code) => {
     if (code !== 0) {
         log.info("Restarting Project immediately...");
-        startProject(); // Instant restart
+        setTimeout(startProject, 1000); // 1s delay for stability
     }
 });
 
